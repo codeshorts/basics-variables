95a3bc81c0373a4e758b45a29b78925710902f59
Zachary Shorts <shorts23@gmail.com>
2020-03-21 16:10:55 -0500

    adding first basics test

---
+++ basicVariables.js
@@ -0,0 +1,11 @@
+const basicVariables = () => {
+  // Create a variable here and give it a value of "Hello World!"
+  // let {name of variable} = "Hello World!";
+  let variable = "Hello World!"
+
+  // Return the variable below 
+  // return {name of variable};
+  return variable;
+}
+
+module.exports = basicVariables;
